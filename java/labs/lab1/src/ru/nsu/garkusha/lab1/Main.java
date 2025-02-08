780ec617cffe4b319e2a2efecc9183289da0d654
k.garkusha <k.garkusha@g.nsu.ru>
2025-02-08 17:06:41 +0700

    lab1, доделала (итерируем, пока не выиграем)

--- java/labs/lab1/src/ru/nsu/garkusha/lab1/Main.java
+++ java/labs/lab1/src/ru/nsu/garkusha/lab1/Main.java
@@ -9,28 +9,31 @@ class Main {
         int[] numbers = generator.numberGeneration();
         int[] guess = new int[4];
 
+        int bulls = 0;
         Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 4; i++){
-            guess[i] = scanner.nextInt();
-        }
-        GuessChecker Checker = new GuessChecker();
-        Checker.checker(guess, numbers);
-        if(Checker.bulls == 4){
-            System.out.println("вы угадали!:)");
-        }
-        else {
+
+        while(bulls != 4) {
+
+            for(int i = 0; i < 4; i++){
+                guess[i] = scanner.nextInt();
+            }
+            GuessChecker Checker = new GuessChecker();
+            Checker.checker(guess, numbers);
             System.out.println("вы не угадали!:(");
             System.out.println("коров: " + Checker.cows + " быков: " + Checker.bulls);
 
             System.out.println("загаданное число: ");
-            String numbersPrint = Arrays.toString(numbers).replace("[", "").replace("]", "").replace(",", " ");
+            String numbersPrint = Arrays.toString(numbers).replaceAll("[^1-9]", "");
             System.out.println(numbersPrint);
 
 
             System.out.println("ваша догадка: ");
-            String guessPrint = Arrays.toString(guess).replace("[", "").replace("]", "").replace(",", " ");
+            String guessPrint = Arrays.toString(guess).replaceAll("[^1-9]", "");
             System.out.println(guessPrint);
+            bulls = Checker.bulls;
 
         }
+        System.out.println("вы угадали!:)");
+
     }
 }
